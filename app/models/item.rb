5372f66102efda2aae59856e5cae170724259bf6
yukyamam <yamamoto.yuka.0411@gmail.com>
2021-09-20 21:20:56 +0900

    Itemモデルの作成

---
+++ app/models/item.rb
@@ -0,0 +1,12 @@
+class Item < ApplicationRecord
+  validates :item_name,          presence: true
+  validates :description,        presence: true
+  validates :category_id,        presence: true
+  validates :condition_id,       presence: true
+  validates :delivery_fee_id,    presence: true
+  validates :prefecture_id,      presence: true
+  validates :scheduled_delivery, presence: true
+  validates :price,              presence: true
+
+  belongs_to :user
+end
