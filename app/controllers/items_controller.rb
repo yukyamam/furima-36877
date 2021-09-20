5d381ca25d6a8fc44a9cbad46d9541ce24bca460
yukyamam <yamamoto.yuka.0411@gmail.com>
2021-09-20 22:08:28 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
+
   def index
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :delivery_fee_id, :prefecture_id. :scheduled_delivery, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,5 @@ class Item < ApplicationRecord
   validates :price,              presence: true
 
   belongs_to :user
+  has_one_attached :image
 end
