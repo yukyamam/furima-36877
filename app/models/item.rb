76285219dba37e88eb9d5456753a6fdf1314faa2
yukyamam <yamamoto.yuka.0411@gmail.com>
2021-09-21 19:10:38 +0900

    単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
   validates :delivery_fee_id,       presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id,         presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :scheduled_delivery_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price,                 presence: true
+  validates :price,                 presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
 
   belongs_to :user
   has_one_attached :image
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+    # image                   {}
+    item_name               {'ポケモン新作ゲームカセット'}
+    description             {'ルビー・サファイアの復刻版です！'}
+    category_id             {'6'}
+    condition_id            {'2'}
+    delivery_fee_id         {'3'}
+    prefecture_id           {'12'}
+    scheduled_delivery_id   {'4'}
+    price                   {'5000'}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
