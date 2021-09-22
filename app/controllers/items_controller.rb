933f7461e342b0cd3fa928355143835c543ea39d
yukyamam <yamamoto.yuka.0411@gmail.com>
2021-09-22 20:27:29 +0900

    DRYを意識したコード(修正)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,8 +45,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    # unless user_signed_in?
-    @item = Item.find(params[:id])
     unless current_user.id == @item.user_id
       redirect_to action: :index
     end
