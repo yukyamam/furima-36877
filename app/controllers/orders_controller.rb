0b7e7c87e64631a3f52b8d49139d26dfd1a2bb91
yukyamam <yamamoto.yuka.0411@gmail.com>
2021-09-27 17:18:01 +0900

    再々修正(提出なし)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,10 +7,6 @@ class OrdersController < ApplicationController
     @order_address = OrderAddress.new
   end
 
-  def new
-    @order_address = OrderAddress.new
-  end
-
   def create
     @order_address = OrderAddress.new(order_address_params)
     if @order_address.valid?
