93df4e96da0ed7562a5dc494d84e566fe6dc495d
yukyamam <yamamoto.yuka.0411@gmail.com>
2021-09-28 10:18:33 +0900

    最終修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,11 +5,12 @@ class Category < ActiveHash::Base
     { id: 3, name: 'メンズ' },
     { id: 4, name: 'ベビー・キッズ' },
     { id: 5, name: 'インテリア・住まい・小物' },
-    { id: 6, name: 'おもちゃ・ホビー・グッズ' },
-    { id: 7, name: '家電・スマホ・カメラ' },
-    { id: 8, name: 'スポーツ・レジャー' },
-    { id: 9, name: 'ハンドメイド' },
-    { id: 10, name: 'その他' }
+    { id: 6, name: '本・音楽・ゲーム'}
+    { id: 7, name: 'おもちゃ・ホビー・グッズ' },
+    { id: 8, name: '家電・スマホ・カメラ' },
+    { id: 9, name: 'スポーツ・レジャー' },
+    { id: 10, name: 'ハンドメイド' },
+    { id: 11, name: 'その他' }
   ]
 
   include ActiveHash::Associations
